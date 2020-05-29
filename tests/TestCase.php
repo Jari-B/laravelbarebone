57e32665485e64a2a400f4943400ab2f5cb223d7
Jari-B <jarivdberg@gmail.com>
2020-05-29 15:32:38 +0200

    chore(test): update testcase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,51 @@
 
 namespace Tests;
 
+use App\Models\Users\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    protected $globalUser;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        Artisan::call('migrate:fresh --seed --env=testing');
+
+        $this->enableForeignKeys();
+        $this->globalUser = factory(User::class)->create();
+    }
+
+    public function tearDown(): void
+    {
+        Artisan::call('migrate:reset --env=testing');
+        parent::tearDown();
+    }
+
+    /**
+     * Clears Laravel Cache.
+     */
+    protected function clearCache()
+    {
+        $commands = ['clear-compiled', 'cache:clear'];
+        foreach ($commands as $command) {
+            \Illuminate\Support\Facades\Artisan::call($command);
+        }
+    }
+
+    /**
+     * Enables foreign keys.
+     *
+     * @return void
+     */
+    public function enableForeignKeys()
+    {
+        if (DB::connection() instanceof \Illuminate\Database\SQLiteConnection) {
+            DB::statement(DB::raw('PRAGMA foreign_keys=1'));
+        }
+    }
 }
